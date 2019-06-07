9c753cc4ae557907f237622ea616586260f35984
swappysh <swapnil@nference.net>
2019-06-07 15:36:22 +0530

    Moved templates to separate package

--- wiki.go
+++ wiki.go
@@ -8,7 +8,7 @@ import (
 	"regexp"
 )
 
-var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+var templates = template.Must(template.ParseFiles("./tmpl/edit.html", "./tmpl/view.html"))
 var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
 
 type Page struct {
